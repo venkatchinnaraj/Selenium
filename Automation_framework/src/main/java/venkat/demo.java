48cc376595be3f52dfc5fb7759119ce846ab837b
Admin <Admin@DESKTOP-G4F1SD9>
2025-07-01 13:14:15 +0530

    Demo

---
+++ Automation_framework/src/main/java/venkat/demo.java
@@ -0,0 +1,14 @@
+package venkat;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void launch() {
+		WebDriver driver=new ChromeDriver();
+		
+	}
+
+}
