94835466c69bab28187bd24065825971695309b6
Admin <Admin@DESKTOP-G4F1SD9>
2025-07-02 11:05:30 +0530

    demo2

--- dummy/src/test/java/dummy_D/demo.java
+++ dummy/src/test/java/dummy_D/demo.java
@@ -1,5 +1,7 @@
 package dummy_D;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -9,8 +11,10 @@ public class demo {
 	public void lainch() {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://github.com/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.close();
-		driver.quite();
+		
 		
 	}
 
